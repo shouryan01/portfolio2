f97fe8da7ce05397bd1c8aeab4d097ee5e4aecd1
Shouryan Nikam <dev.shouryan@gmail.com>
2025-02-09 11:25:29 -0800

    add link to new blog

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
+  { href: '/blogtest', title: 'NewBlog' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' }
 ]
